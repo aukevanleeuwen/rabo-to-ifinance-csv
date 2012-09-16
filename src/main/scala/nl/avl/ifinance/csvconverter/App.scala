6ed5de4ca7ea429d39bfab827ab75dad4cf30eb0
Aukek van Leeuwen <aukevanleeuwen@gmail.com>
2012-09-16 23:33:03 +0200

    Add commandline options for clarity

--- src/main/scala/nl/avl/ifinance/csvconverter/App.scala
+++ src/main/scala/nl/avl/ifinance/csvconverter/App.scala
@@ -3,15 +3,23 @@ package nl.avl.ifinance.csvconverter
 import java.io.FileReader
 import java.io.FileWriter
 
-/**
- * Hello world!
- *
- */
+case class Config(input: String = "", output: String = "", verbose: Boolean = false)
+
 object App {
-  def main(args: Array[String]): Unit = {
-    val reader = new FileReader(args(0))
-    val writer = new FileWriter(args(1))
-    
-    new CsvRecordConverter(reader, writer).convert
-  }
+    def main(args: Array[String]): Unit = {
+        val parser = new scopt.immutable.OptionParser[Config]("java -jar <programjar>", "Rabobank to iFinance Converter") {
+            def options = Seq(
+                booleanOpt("v", "verbose", "Be more verbose") { (v: Boolean, c: Config) => c.copy(verbose = v) },
+                arg("<input>", "Input file") { (v: String, c: Config) => c.copy(input = v) },
+                arg("<output>", "Output file") { (v: String, c: Config) => c.copy(input = v) }
+            )
+        }
+
+        parser.parse(args, Config()) map { config =>
+            val reader = new FileReader(args(0))
+            val writer = new FileWriter(args(1))
+
+            new CsvRecordConverter(reader, writer).convert
+        }
+    }
 }
